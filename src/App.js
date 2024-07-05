b237134901593a3cb75a00c09685a0a2eb3893a3
Herbert Nordson <herbert_nordson@hotmail.com>
2024-07-05 01:04:42 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { CARS_BRAND_MOCK } from "./utils/constants";
 
 function App() {
+  const [cars, setCars] = useState([]);
+
+  function handleCar() {
+    const newCars = CARS_BRAND_MOCK.cars.map((item) => item);
+    setCars(newCars);
+  }
+
+  useEffect(() => {
+    handleCar();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {cars.length > 0 &&
+        cars.map((car) => (
+          <article key={car.id}>
+            <img src="" alt="" />
+            <section>
+              <header>
+                <h1 className="font-semibold text-xl">{car.nome_modelo}</h1>
+                <p className="font-medium opacity-75">{car.ano}</p>
+              </header>
+              <main>
+                <div>
+                  <p>Portas: {car.num_portas}</p>
+                  <p>Combustivel: {car.combustivel}</p>
+                </div>
+                <hr />
+                <div>
+                  <p>Cor: {car.cor}</p>
+                  <p>Preço: {car.valor}</p>
+                </div>
+              </main>
+              <button>Comprar</button>
+            </section>
+          </article>
+        ))}
     </div>
   );
 }
